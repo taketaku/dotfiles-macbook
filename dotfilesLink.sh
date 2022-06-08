6e06e6654b95d1b5f812816ace7bea5641d8f3f8
taketaku <vbvbsp48@gmail.com>
2022-06-08 17:47:26 +0900

    update

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,8 +3,7 @@ ln -fns ~/dotfiles/Brewfile ~/Brewfile
 ln -fns ~/dotfiles/.gitconfig ~/.gitconfig
 ln -fns ~/dotfiles/.config/fish ~/.config/fish
 ln -fns ~/dotfiles/.config/nvim ~/.config/nvim
-ln -fns ~/dotfiles/.config/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
-ln -fns ~/dotfiles/.config/karabiner/assets ~/.config/karabiner/assets
+ln -fns ~/dotfiles/.config/karabiner ~/.config/karabiner
 ln -fns ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 # vscode
