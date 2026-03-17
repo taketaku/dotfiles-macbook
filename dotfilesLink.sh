9e7ca23940b4b9bfb578995131571c3627934267
taketaku <vbvbsp48@gmail.com>
2026-03-17 15:08:32 +0900

    update

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -6,15 +6,13 @@ ln -fns ~/dotfiles/home/.gitconfig ~/.gitconfig
 ln -fns ~/dotfiles/home/.gitignore_global ~/.gitignore_global
 ln -fns ~/dotfiles/home/.tmux.conf ~/.tmux.conf
 ln -fns ~/dotfiles/home/.ideavimrc ~/.ideavimrc
+ln -fns ~/dotfiles/home/hammerspoon ~/.hammerspoon
 
 # .config/ -> ~/.config/
 ln -fns ~/dotfiles/.config/fish ~/.config/fish
 ln -fns ~/dotfiles/.config/ghostty ~/.config/ghostty
 ln -fns ~/dotfiles/.config/nvim ~/.config/nvim
 ln -fns ~/dotfiles/.config/karabiner ~/.config/karabiner
-
-# hammerspoon
-ln -fns ~/dotfiles/home/hammerspoon ~/.hammerspoon
 
 # vscode
 ln -fns ~/dotfiles/.config/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
