7b5bed28a42886c8ee042236ca51c88f9dd2ef62
taketaku <vbvbsp48@gmail.com>
2026-03-17 14:04:37 +0900

    構成変更

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,17 +1,19 @@
 #! /bin/bash
-ln -fns ~/dotfiles/Brewfile ~/Brewfile
-ln -fns ~/dotfiles/.gitconfig ~/.gitconfig
+
+# home/ -> ~/
+ln -fns ~/dotfiles/home/Brewfile ~/Brewfile
+ln -fns ~/dotfiles/home/.gitconfig ~/.gitconfig
+ln -fns ~/dotfiles/home/.gitignore_global ~/.gitignore_global
+ln -fns ~/dotfiles/home/.tmux.conf ~/.tmux.conf
+ln -fns ~/dotfiles/home/.ideavimrc ~/.ideavimrc
+
+# .config/ -> ~/.config/
 ln -fns ~/dotfiles/.config/fish ~/.config/fish
 ln -fns ~/dotfiles/.config/ghostty ~/.config/ghostty
 ln -fns ~/dotfiles/.config/nvim ~/.config/nvim
 ln -fns ~/dotfiles/.config/karabiner ~/.config/karabiner
-ln -fns ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -fns ~/dotfiles/.gitignore_global ~/.gitignore_global
 
 # vscode
 ln -fns ~/dotfiles/.config/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 ln -fns ~/dotfiles/.config/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -fns ~/dotfiles/.config/vscode/tasks.json ~/Library/Application\ Support/Code/User/tasks.json
-
-# android studio
-ln -fns ~/dotfiles/.ideavimrc ~/.ideavimrc
